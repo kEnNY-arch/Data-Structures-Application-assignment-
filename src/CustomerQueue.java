62aa30d6e8aaef25b263af65b48f44683a53998c
Kennedy Odhiambo <146546368+kEnNY-arch@users.noreply.github.com>
2024-07-11 18:46:06 +0300

    CustomerQueue.java

--- src/CustomerQueue.java
+++ src/CustomerQueue.java
@@ -2,24 +2,20 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class CustomerQueue {
-    // Queue to manage customers
     private Queue<String> customers;
 
     public CustomerQueue() {
         customers = new LinkedList<>();
     }
 
-    // Add a customer to the queue
     public void addCustomer(String customer) {
         customers.add(customer);
     }
 
-    // Serve a customer
     public String serveCustomer() {
         return customers.poll();
     }
 
-    // Display all customers in the queue
     public void displayQueue() {
         System.out.println("Customers in queue:");
         for (String customer : customers) {
